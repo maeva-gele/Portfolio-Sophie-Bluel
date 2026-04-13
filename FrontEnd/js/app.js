58dd6bbd5ce885ed1e3597f3b01ec5c5786bbb9f
Maeva Gele <maeva.l.gele@gmail.com>
2026-04-13 22:31:38 +0200

    création du fichier JavaScript et création de la fonction appel

---
+++ FrontEnd/js/app.js
@@ -0,0 +1,37 @@
+//RECUPERER LES DONNEES DANS L'API
+
+async function getWorks() {
+  const url = "http://localhost:5678/api/works" //lien de l'API pour la lecture machine
+  try {
+    const reponse = await fetch(url)
+    if (!reponse.ok) {
+      throw new Error(`Statut de réponse : ${reponse.status}`)
+    }
+
+    const json = await reponse.json()
+    console.log(json)
+    setFigure(json)
+
+    for (let index = 0; index < json.length; index++) {
+        setFigure(json[index])
+    }
+  } catch (erreur) {
+    console.error(erreur.message)
+  }
+}  
+getWorks() 
+
+//CREER LES <figure> DANS LE HTML 
+const gallery = document.querySelector(".gallery")
+
+function setFigure(data) {
+
+    console.log("Processing item:", data)
+
+    const figure = document.createElement("figure")
+
+    figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
+                        <figcaption>${data.title}</figcaption>`
+    
+    gallery.appendChild(figure)
+}
